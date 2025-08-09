595f6bd9504a8280014945e5b762645b779c50f5
IT22220288@my.sliit.lk <156440372+RusiruWaduge@users.noreply.github.com>
2025-08-09 20:21:42 +0530

    updated

--- frontend/taskapp/src/App.jsx
+++ frontend/taskapp/src/App.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import AddTask from './AddTask';
 import EditTask from './EditTask';
 import Login from './Login';
 import Register from './Register';
 import './App.css'; 
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/add" element={<AddTask />} />
-        <Route path="/edit/:id" element={<EditTask />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/add" element={<AddTask />} />
+      <Route path="/edit/:id" element={<EditTask />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
 export default App;
+
